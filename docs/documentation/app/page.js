cd53e7a61a93c08b997a72d13e0bbc925eee2fd3
Etienne ANNE <etienne.anne@icloud.com>
2023-11-06 14:53:12 +0100

    fix documentation build

--- docs/documentation/app/page.js
+++ docs/documentation/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/nextjs-github-pages/vercel.svg"
+              src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
